5fe402a74af23595b7f39b22533401394e673dc7
imysh578 <imysh578@gmail.com>
2022-08-17 11:15:35 +0900

    ADD: Async Boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from "react"
-import { Route, Routes } from "react-router-dom"
-import Navbar from "./components/Navbar"
-import OpenNavbar from "./components/OpenNavbar"
-import HomePage from "./pages/HomePage"
-import Footer from "./components/Footer"
-import MainContainer from "./components/MainContainer"
-import HashPage from "./pages/HashPage"
-import BlockPage from "./pages/BlockPage"
+import React, { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import OpenNavbar from "./components/OpenNavbar";
+import HomePage from "./pages/HomePage";
+import Footer from "./components/Footer";
+import MainContainer from "./components/MainContainer";
+import HashPage from "./pages/HashPage";
+import BlockPage from "./pages/BlockPage";
+import AsyncBoundary from "./components/AsyncBoundary/AsyncBoundary";
+import Loading from "./components/Loading";
+import Error from "./components/Error";
 
 function App() {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -14,36 +17,40 @@ function App() {
 		setIsMenuOpen((prev) => !prev);
 	};
 
-  const changeNav = () => {
+	const changeNav = () => {
 		if (window.innerWidth >= 900) {
-			setIsMenuOpen(false)
+			setIsMenuOpen(false);
 		}
 	};
 
 	useEffect(() => {
 		window.addEventListener("resize", changeNav);
-    return () => {
-      window.removeEventListener("resize", changeNav)
-    }
+		return () => {
+			window.removeEventListener("resize", changeNav);
+		};
 	}, []);
 
-
-  return (
-    <>
-      <OpenNavbar isOpen={isMenuOpen} toggle={handleOnClickOpenMenu} />
-      <Navbar toggle={handleOnClickOpenMenu}/>
+	return (
+		<>
+			<OpenNavbar isOpen={isMenuOpen} toggle={handleOnClickOpenMenu} />
+			<Navbar toggle={handleOnClickOpenMenu} />
 			<MainContainer>
-				<Routes>
-					<Route path="/" >
-						<Route index element={<HomePage/>}/>
-						<Route path="hash" element={<HashPage/>} />
-						<Route path="block" element={<BlockPage/>} />
-					</Route>
-				</Routes>
+				<AsyncBoundary
+					ErrorFallback={(arg) => <Error error={arg.error} />}
+					SuspenseFallback={<Loading />}
+				>
+					<Routes>
+						<Route path="/">
+							<Route index element={<HomePage />} />
+							<Route path="hash" element={<HashPage />} />
+							<Route path="block" element={<BlockPage />} />
+						</Route>
+					</Routes>
+				</AsyncBoundary>
 			</MainContainer>
-      <Footer />
-    </>
-  )
+			<Footer />
+		</>
+	);
 }
 
-export default App
+export default App;
